4a14939c88bc573f0cd6506a9f123e60d8490cf2
khanh <khanhtmd99@gmail.comKhanhtmd@1997>
2021-12-07 17:34:58 +0700

    update

--- src/reports/components/index.js
+++ src/reports/components/index.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { FormBuilder as FormBuilderIo, Formio } from "react-formio";
 import '../assets/css/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ViewPDF from "./viewPDF";
+import View from "./view";
 import { formIoData } from "../core/data";
 
 export default function ReportAuto() {
@@ -13,59 +13,80 @@ export default function ReportAuto() {
   const [isView, setIsView] = useState(false)
   const data = [
     {
-      "id": 25,
-      "riyousyaId": 270,
-      "riyousyaName": "Name ACB",
-      "riyousyaFurigana": "アアア",
-      "riyousyaGender": 1,
-      "riyousyaBirthDay": "1946-08-06",
-      "sid": 59,
-      "hyoukaDate": "2021-12-06",
-      "makeDate": "2021-12-06",
-      "tantoId": 34,
-      "tantoName": "中本　峰子",
-      "tantoFurigana": "ナカモトミネコ",
-      "isBedsore": "1",
-      "bedsorePartOther": "2",
-      "bedsorePartFreeDescription": "fghfghfg",
-      "bedsoreDateOfOnset": null,
-      "isPastBedsore": null,
-      "pastBedsorePart": "",
-      "pastBedsorePartFreeDescription": null,
-      "dailyDegree": null,
-      "adlBathe": "1",
-      "adlDietaryIntake": null,
-      "adlDressingUpper": null,
-      "adlDressingLower": "1",
-      "kihonTurningOver": null,
-      "kihonSittingContinuous": "0",
-      "kihonTransfer": null,
-      "kihonKeepStanding": null,
-      "haisetuUrinationIncontinence": null,
-      "haisetuDefecationIncontinence": null,
-      "haisetuBalloonCatheter": null,
-      "isBedsoreWithinThreeMonths": null,
-      "depthEvaluation": "1",
-      "leachateEvaluation": null,
-      "sizeEvaluation": null,
-      "infectionEvaluation": null,
-      "granulationEvaluation": null,
-      "necroticTissueEvaluation": null,
-      "pocketSizeEvaluation": null,
-      "mattersRelatedJobs": "fgh",
-      "evaluationInterval": null,
-      "removedPressureOnBed": null,
-      "removedPressureOnChair": null,
-      "skinCare": null,
-      "improvedNutrition": null,
-      "rehabilitation": null,
-      "bedsoreOther": null,
-      "kakuteif": 0,
-      "table": {
-        "rowOne": 'row 1',
-        "rowTwo": 'row 2'
-      }
+      adlBathe: null,
+      adlDietaryIntake: null,
+      adlDressingLower: null,
+      adlDressingUpper: null,
+      bedsoreDateOfOnset: null,
+      bedsoreOther: null,
+      bedsorePartFreeDescription: "ưerew",
+      bedsorePartOther: null,
+      dailyDegree: null,
+      depthEvaluation: null,
+      evaluationInterval: null,
+      granulationEvaluation: null,
+      haisetuBalloonCatheter: null,
+      haisetuDefecationIncontinence: null,
+      haisetuUrinationIncontinence: null,
+      hyoukaDate: "2021-12-07",
+      id: 31,
+      improvedNutrition: null,
+      infectionEvaluation: null,
+      isBedsore: null,
+      isBedsoreWithinThreeMonths: null,
+      isPastBedsore: null,
+      kakuteif: 0,
+      kihonKeepStanding: null,
+      kihonSittingContinuous: null,
+      kihonTransfer: null,
+      kihonTurningOver: null,
+      leachateEvaluation: null,
+      makeDate: "2021-12-07",
+      mattersRelatedJobs: null,
+      necroticTissueEvaluation: null,
+      pastBedsorePart: "",
+      pastBedsorePartFreeDescription: null,
+      pocketSizeEvaluation: null,
+      rehabilitation: null,
+      removedPressureOnBed: null,
+      removedPressureOnChair: null,
+      riyousyaBirthDay: "1946-08-06",
+      riyousyaFurigana: "アアア",
+      riyousyaGender: 1,
+      riyousyaId: 270,
+      riyousyaName: "あああ",
+      sid: 59,
+      sizeEvaluation: null,
+      skinCare: null,
+      tantoFurigana: "ナカモトミネコ",
+      tantoId: 34,
+      tantoName: "中本　峰子",
     }
+  ]
+
+  const dataMaster = [
+    {
+      adlnyuyoku: "0:自分で行っていない;1:自分で行っている",
+      catheter: "0:無し;1:有り",
+      enshou: "0:局所の炎症徴候なし（i0）;1:局所の炎症徴候あり(創周囲の発赤，腫脹，熱感，疼痛)（i1）;2:臨床的定着疑い（創面にぬめりがあり、浸出液が多い。肉芽があれば、浮腫性で脆弱など）（I3C）;3:局所の明らかな感染徴候あり(炎症徴候，膿，悪臭など) （I3）;4:全身的影響あり(発熱など)（I9）",
+      esi: "0:壊死組織なし（n0）;1:柔らかい壊死組織あり（N3）;2:硬く厚い密着した壊死組織あり（N6）",
+      jokusobui: "1:仙骨部;2:坐骨部;3:尾骨部;4:腸骨部;5:大転子部;6:踵部;7:その他",
+      jokusohukasa: "0:皮膚損傷・発赤なし（d0）;1:持続する発赤（d1）;2:真皮までの損傷（d2）;3:皮下組織までの損傷（D3）;4:皮下組織を越える損傷 （D4）;5:関節腔，体腔に至る損傷 （D5）;6:深部損傷褥瘡（DTI)疑い（DDTI）;7:壊死組織で覆われ深さ判定が不能（DU）",
+      jokusoookisa: "0:皮膚損傷なし（s0）;1:4未満（s3）;2:4以上16未満（s6）;3:16以上36未満（s8）;4:36以上64未満（s9）;5:64以上100未満（s12）;6:100以上（S15）",
+      jokusopocket: "0:ポケットなし（p0）;1:4未満（P6）;2:4以上16未満（P9）;3:16以上36未満（P12）;4:36以上（P24）",
+      jokusoumu: "0:無し;1:有り",
+      kioreki: "0:無し;1:有り",
+      koui: "0:自分で行っていない;1:自分で行っている",
+      negaeri: "0:自分で行っていない;1:自分で行っている",
+      nikuga: "0:創が治癒した場合、創の浅い場合、深部損傷褥瘡（DTI)疑いの場合（g0）;1:良性肉芽が創面の90%以上を占める（g1）;2:良性肉芽が創面の50%以上90%未満を占める（g3）;3:良性肉芽が創面の10%以上50%未満を占める（G4）;4:良性肉芽が創面の10%未満を占める（G5） ;5:良性肉芽が全く形成されていない（G6）",
+      ritsui: "0:自分で行っていない;1:自分で行っている",
+      shokuzisesshu: "0:自分で行っていない;1:自分で行っている;2:対象外",
+      sikkin: "0:無し;1:有り;2:対象外",
+      sinshutueki: "0:なし（e0）;1:少量：毎日のドレッシング交換を要しない（e1）;2:中等量：1日1回のドレッシング交換を要する（e3）;3:多量：1日2回以上のドレッシング交換を要する（E6）",
+      zai: "0:自分で行っていない;1:自分で行っている",
+      ziritsudo: ";自立;J1;J2;A1;A2;B1;B2;C1;C2",
+    }
+
   ]
   const printResult = () => {
     if (formData.components[0].class === 'notDisplay' && formData.components.length === 1) setIsView(false)
@@ -88,7 +109,7 @@ export default function ReportAuto() {
         <button className="green" onClick={printResult} style={{ marginRight: '20px' }}>
           Save Form
         </button>
-        <ViewPDF dataTableReport={result} dataAPI={data && data.length > 0 ? data : data === []} isView={isView} />
+        <View dataTableReport={result} dataAPI={data && data.length > 0 ? data : data === []} dataMaster={dataMaster} isView={isView} />
       </div>
       <div>
         <FormBuilderIo
---
+++ src/reports/components/view.js
@@ -0,0 +1,241 @@
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { useEffect, useState } from "react";
+import {
+  _CENTER,
+  _HIDEBORDER,
+  _HTMLELEMENT,
+  _LEFT,
+  _NOTDISPLAY,
+  _RIGHT,
+  _TABLE,
+  _TAGH1,
+  _TAGH2,
+  _TAGH3,
+  _TAGH4,
+  _TAGH5,
+  _TAGHP,
+  _TITLETABLE,
+  _TRUESTRING
+} from '../contants/index';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+export default function View(props) {
+  const { dataTableReport, dataAPI, dataMaster, isView } = props;
+  const [pdfData, setPDFData] = useState({});
+
+  const widthColumns = [
+    'width5', 'width10', 'width15', 'width20', 'width25', 'width30', 'width35', 'width40', 'width45', 'width50',
+  ];
+
+  const genderOption = [
+    {
+      label: "男",
+      value: 1,
+    },
+    {
+      label: "女",
+      value: 0,
+    },
+  ];
+
+  const attributes = {
+    margin: '0.5',
+    width: '10%',
+    selectbox: 'true',
+    birthday: 'true',
+    sex: 'true'
+  }
+
+  function calculateAge(dob) {
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms);
+
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+  }
+
+  function formatDateJapan(dateStr) {
+    if (!dateStr) return "";
+
+    let dob = new Date(dateStr);
+    let age = calculateAge(dob);
+    return (
+      dob.getFullYear() +
+      "年 " +
+      (dob.getMonth() + 1) +
+      "月 " +
+      dob.getDate() +
+      "日生（" +
+      age +
+      "歳）"
+    );
+  }
+
+  function generateCheckBoxString(options, optionSelected) {
+    let result = "";
+    options.forEach(item => {
+      if (item.value === optionSelected) {
+        result += "■" + item.label;
+      } else {
+        result += "□" + item.label;
+      }
+    })
+    return result;
+  }
+
+  function getMargin(rawItem) {
+    let margin = []
+    if (rawItem.attributes && rawItem.attributes.margin !== undefined) {
+      margin = rawItem.attributes.margin.split(',').map(Number)
+    } else margin = []
+    return margin
+  }
+
+  function renderDataColumns(data, dataChildItem) {
+    let columns = []
+    data.map(item => {
+      return columns.push(switchHtmlElement(dataChildItem, item))
+    })
+
+    return {
+      columns: columns
+    }
+  }
+
+  function switchColumnsTable(dataChildItem, rawItem, dataMaster) {
+    for (let i = 0; i < rawItem.rows.length; i++) {
+      //check content not value => content = ''
+      // eslint-disable-next-line
+      rawItem.rows[i]?.map(item => {
+        if (item.components.length === 0) item.components.push({ content: '' })
+      });
+    }
+    const data = rawItem.rows.map(row => row.map(child => Array.from(child.components, x => {
+      let width = (x.attributes && x.attributes.width !== '') ? x.attributes.width : '*';
+      let propertyValue = (x.attributes && x.attributes.birthday === 'true') ? formatDateJapan(dataChildItem[x.key]) : dataChildItem[x.key];
+      let alignment = x.className && x.className.indexOf(_CENTER) > -1 ? _CENTER : x.className && x.className.indexOf(_RIGHT) > -1 ? _RIGHT : _LEFT;
+      let textColumns = (x.attributes && x.attributes.selectbox === 'true' && x.attributes.sex === 'true') ? generateCheckBoxString(genderOption, propertyValue) : dataChildItem[x.key];
+      let arr = [
+        {
+          id: x.content,
+          name: x.content
+        },
+        {
+          id: textColumns,
+          name: textColumns
+        }]
+      const unique = arr.filter((v, i, a) => a.findIndex(t => (t.id === v.id && t.name === v.name)) === i)
+      let result = {
+        text: unique.length === 1 ? unique[0].name : (unique[0].name + ' ' + unique[1].name),
+        width,
+        alignment,
+        textColumns,
+        propertyValue
+      };
+
+      return result
+
+    })).reduce((prev, next) => {
+      return prev.concat(next);
+    }))
+
+    if (rawItem.label === 'columns') {
+      return renderDataColumns(data[0], dataChildItem)
+    }
+
+
+
+  }
+
+  function switchHtmlElement(dataChildItem, rawItem, dataMaster) {
+    let fontSize = 14;
+    if (rawItem.tag === _TAGH1) fontSize = 32
+    else if (rawItem.tag === _TAGH2) fontSize = 24
+    else if (rawItem.tag === _TAGH3) fontSize = 18.72
+    else if (rawItem.tag === _TAGH4 || rawItem.tag === _TAGHP) fontSize = 16
+    else if (rawItem.tag === _TAGH5) fontSize = 13.28
+    else fontSize = 10.72;
+
+    let style = {
+      fontSize: fontSize,
+    }
+
+    return { text: rawItem.text, alignment: rawItem.alignment, width: rawItem.width, ...style }
+
+    // if (dataChildItem[rawItem.key] !== undefined && dataChildItem[rawItem.key] !== null) {
+    //   return {
+    //     rawItem
+    //     // ...style,
+
+    //   }
+    // } else {
+    //   return {
+    //     text: rawItem.content,
+    //     // ...style
+    //   }
+    // }
+
+    console.log();
+
+  }
+
+  function renderDataSwitch(dataChildItem, rawItem, dataMaster) {
+    let type = rawItem.type;
+    switch (type) {
+      case "htmlelement":
+        return switchHtmlElement(dataChildItem, rawItem, dataMaster)
+      case 'columns':
+        const dataItem = {
+          rows: [],
+          label: 'columns'
+        }
+        dataItem.rows.push(rawItem.columns)
+        return switchColumnsTable(dataChildItem, dataItem, dataMaster)
+
+      default:
+        break;
+    }
+  }
+
+  function drawTable(dataChildItem, rawData, dataMaster) {
+    return renderDataSwitch(dataChildItem, rawData, dataMaster)
+  }
+
+  function drawContent(dataChildItem, rawData, dataMaster) {
+    // eslint-disable-next-line
+    return rawData.map((rawItem) => {
+      // eslint-disable-next-line
+      // if (rawItem.class === _NOTDISPLAY || (rawItem.type && rawItem.type !== _TABLE)) return
+      return drawTable(dataChildItem, rawItem, dataMaster);
+    });
+  }
+
+
+  function buildPDFDefination(rawData) {
+    let dd = {
+      content: dataAPI.map(dataChildItem => drawContent(dataChildItem, rawData, dataMaster)),
+    }
+    return dd;
+  }
+
+  useEffect(() => {
+    const pdfDefination = buildPDFDefination(dataTableReport);
+    setPDFData(pdfDefination)
+    // eslint-disable-next-line
+  }, [dataTableReport])
+
+  const onClickPdfMakeHandler = async (pdfData) => {
+    pdfMake.createPdf(pdfData).open();
+  }
+
+  return (
+    <div>
+      {
+        isView ? <button onClick={() => onClickPdfMakeHandler(pdfData)} >View PDF</button>
+          : null
+      }
+    </div>
+
+  )
+}
